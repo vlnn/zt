b4325403ad8bb27b61921967fd861641dae73346
vlnn <vlnn-github@proton.me>
2026-04-19 15:12:58 +0300

    Make QoL in the reaction example

--- examples/reaction/app/reaction.fs
+++ examples/reaction/app/reaction.fs
@@ -20,6 +20,9 @@ variable round-count
     begin  wait-frame 1+  key?  until
     key swap ;
 
+: wait-for-release  ( -- )   begin wait-frame key? 0= until ;
+: await-any-key     ( -- )   wait-for-release wait-for-key 2drop ;
+
 : check-key     ( shown typed -- correct? )  swap digit>char = ;
 : verdict       ( correct? -- )              if ." correct! " else ." wrong! " then ;
 
@@ -29,8 +32,9 @@ variable round-count
 
 : print-result  ( ms -- )            ." reacted in " u. ." ms" cr ;
 : print-avg     ( -- )               ." average: "   avg-ms u. ." ms" cr ;
+: print-any-key ( -- )               ." PRESS ANY KEY TO CONTINUE" cr ;
 
-: finish-round  ( ms -- )            dup record-round print-result print-avg ;
+: finish-round  ( ms -- )            dup record-round print-result print-avg print-any-key ;
 
 : play-round    ( -- )
     7 0 cls
@@ -39,6 +43,7 @@ variable round-count
     ." >> " pick-digit dup show-digit cr
     wait-for-key >r
     check-key verdict
-    r> frames>ms finish-round ;
+    r> frames>ms finish-round
+    await-any-key ;
 
 : game-loop     ( -- )               begin play-round again ;
